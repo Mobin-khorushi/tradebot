088b4a402c5e616046ecde709d80ccdf46801b18
Mobin.Loseb <mobin.khorushi@gmail.com>
2021-09-24 02:23:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ coins = {
 }
 def order(coin,amount,leverage,position):
     try:
-        if(coins[coin] != position){
+        if coins[coin] != position :
             result = request_client.change_margin_type(symbol="BTCUSDT", marginType=FuturesMarginType.ISOLATED)
             print("Margin Type: " + result)
             result = request_client.cancel_all_orders(coin)
             print("Order cancel: " + result)
             result = request_client.change_initial_leverage(symbol="BTCUSDT", leverage=10)
             print("Leverage: " + result)
-        }
+        
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
