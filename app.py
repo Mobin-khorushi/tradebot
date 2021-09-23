34d8f7621a6ccd9a95f1d6fad84a1c0640da84bb
Mobin.Loseb <mobin.khorushi@gmail.com>
2021-09-24 02:10:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,7 @@ request_client = RequestClient(api_key=config.API_KEY, secret_key=config.API_SEC
 
 def order():
     try:
-        result = request_client.get_account_information()
-        print("canDeposit: ", result.canDeposit)
-        print("canWithdraw: ", result.canWithdraw)
-        print("feeTier: ", result.feeTier)
-        print("maxWithdrawAmount: ", result.maxWithdrawAmount)
-        print("totalInitialMargin: ", result.totalInitialMargin)
-        print("totalMaintMargin: ", result.totalMaintMargin)
-        print("totalMarginBalance: ", result.totalMarginBalance)
-        print("totalOpenOrderInitialMargin: ", result.totalOpenOrderInitialMargin)
-        print("totalPositionInitialMargin: ", result.totalPositionInitialMargin)
-        print("totalUnrealizedProfit: ", result.totalUnrealizedProfit)
-        print("totalWalletBalance: ", result.totalWalletBalance)
-        print("updateTime: ", result.updateTime)
-        print("=== Assets ===")
-        PrintMix.print_data(result.assets)
-        print("==============")
-        print("=== Positions ===")
-        PrintMix.print_data(result.positions)
-        print("==============")
+        print('done')
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
